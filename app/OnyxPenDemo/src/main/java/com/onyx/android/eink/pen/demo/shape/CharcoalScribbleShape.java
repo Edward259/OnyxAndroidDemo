fe791eab345ee09c5ddf55a70a646d04ce20aa9f
zzzzwb <de01@onyx-international.com>
2026-06-25 15:02:43 +0800

    fix stroke parameters in charcoal drawing error

--- app/OnyxPenDemo/src/main/java/com/onyx/android/eink/pen/demo/shape/CharcoalScribbleShape.java
+++ app/OnyxPenDemo/src/main/java/com/onyx/android/eink/pen/demo/shape/CharcoalScribbleShape.java
@@ -4,9 +4,12 @@ import com.onyx.android.eink.pen.demo.data.ShapeFactory;
 import com.onyx.android.eink.pen.demo.helper.RendererHelper;
 import com.onyx.android.eink.pen.demo.util.RendererUtils;
 import com.onyx.android.sdk.data.PenConstant;
+import com.onyx.android.sdk.data.note.PenAttrs;
 import com.onyx.android.sdk.data.note.PenTexture;
 import com.onyx.android.sdk.data.note.ShapeCreateArgs;
+import com.onyx.android.sdk.data.note.TiltConfig;
 import com.onyx.android.sdk.data.note.TouchPoint;
+import com.onyx.android.sdk.device.Device;
 import com.onyx.android.sdk.pen.NeoCharcoalPenV2Wrapper;
 import com.onyx.android.sdk.pen.NeoCharcoalPenWrapper;
 import com.onyx.android.sdk.pen.PenRenderArgs;
@@ -15,22 +18,21 @@ import java.util.List;
 
 public class CharcoalScribbleShape extends Shape {
 
-    private static final float CHARCOAL_DISPLAY_SCALE_COMPENSATION = 1.5f;
-
     @Override
     public void render(RendererHelper.RenderContext renderContext) {
         List<TouchPoint> points = touchPointList.getPoints();
         applyStrokeStyle(renderContext);
 
-        float renderStrokeWidth = getRenderStrokeWidth()
-                * CHARCOAL_DISPLAY_SCALE_COMPENSATION;
+        float renderStrokeWidth = getRenderStrokeWidth();
+        ShapeCreateArgs createArgs = createShapeCreateArgs();
 
         PenRenderArgs renderArgs = new PenRenderArgs()
-                .setCreateArgs(new ShapeCreateArgs())
+                .setCreateArgs(createArgs)
                 .setCanvas(renderContext.canvas)
                 .setPenType(ShapeFactory.getCharcoalPenType(texture))
                 .setColor(strokeColor)
                 .setErase(isTransparent())
+                .setTiltEnabled(isTiltEnabled(createArgs))
                 .setPaint(renderContext.paint)
                 .setScreenMatrix(RendererUtils.getPointMatrix(renderContext))
                 .setStrokeWidth(renderStrokeWidth)
@@ -54,5 +56,27 @@ public class CharcoalScribbleShape extends Shape {
                 NeoCharcoalPenWrapper.drawBigStroke(renderArgs);
             }
         }
+    }
+
+    private ShapeCreateArgs createShapeCreateArgs() {
+        return new ShapeCreateArgs()
+                .setPenAttrs(new PenAttrs().setTexture(texture))
+                .setTiltConfig(loadTiltConfig());
+    }
+
+    private TiltConfig loadTiltConfig() {
+        int strokeStyle = ShapeFactory.getStrokeStyle(ShapeFactory.SHAPE_CHARCOAL_SCRIBBLE, texture);
+        float[] parameters = Device.currentDevice().getStrokeParameters(strokeStyle);
+        if (parameters == null || parameters.length < 2) {
+            return null;
+        }
+        return new TiltConfig()
+                .setTiltEnabled(parameters[0] != 0)
+                .setTiltScale(parameters[1]);
+    }
+
+    private boolean isTiltEnabled(ShapeCreateArgs createArgs) {
+        return createArgs.getTiltConfig() != null
+                && createArgs.getTiltConfig().isTiltEnabled();
     }
 }
